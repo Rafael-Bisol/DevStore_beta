f7946860aa41115c7084f42efe07cade5164711f
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-04-02 20:46:31 -0300

    Adicionada consulta a API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=notebooks';
+
+fetch(URL)
+    .then(resposta => resposta.json())
+    .then(dados => {
+        const produtos = dados.results;
+        const listaProdutos = document.createElement('ul');
+        produtos.forEach(produto => {
+            const itemLista = document.createElement('li');
+            itemLista.textContent = produto.title;
+            listaProdutos.appendChild(itemLista);
+        })
+        document.getElementById('lista-produtos').appendChild(listaProdutos);
+    })
+    .catch(deuRuimBerg => {
+        console.log('Corre Berg! Erro: ', deuRuimBerg);
+    });
 
